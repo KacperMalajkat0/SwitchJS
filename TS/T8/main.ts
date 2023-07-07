7a28f5653bb1454d2806d7f5c1ae1c2c201d84ef
Kacper Malajkat <Kacper_Malajkat@epam.com>
2023-07-07 15:51:42 +0200

    TS - T8

---
+++ TS/T8/main.ts
@@ -0,0 +1,4 @@
+import { add, subtract } from "my-module";
+
+const e1 = add(10, 10);
+const e2 = subtract(10, 10);
---
+++ TS/T8/myModule.d.ts
@@ -0,0 +1,6 @@
+declare module "my-module" {
+    function add(a: number, b: number): number;
+    function subtract(a: number, b: number): number;
+    
+    export {add, subtract}
+}
